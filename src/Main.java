4903afe90a65208a8e19c7a620758d61283e8e89
TymonScibiorski <tym9128@gmail.com>
2024-12-23 14:24:17 +0100

    Planned the way the code will work, just wrote comments

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+/*
+SCRABBLE BOT 2
+
+Scrabble bot 2 will take as INPUT:
+ - A 2D array, which will be 15 by 15 and will represent the board.
+ - Player's Letters
+
+The board will be scanned row by row, column by column and return the word which will yield the most points.
+
+Scrabble bot 2 will OUTPUT:
+ - The 2D array, which was inputted, with the found word added in capital letters.
+ - The amount of points the returned word would yield.
+ */
+
+/*
+The code would use functions from Scrabble bot 1, such as: Butts, patternMatcher, doLettersMatch and a list of points for a letter and the corresponding letter.
+ */
