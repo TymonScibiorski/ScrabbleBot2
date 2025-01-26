ba7cc1fbdaceab28c899491509b7a882ab3f393e
TymonScibiorski <tym9128@gmail.com>
2025-01-26 23:55:12 +0100

    Changed pointsForLetter and pointsForWord. PointsForWord will have to know how many playaLetters are used to add 50 points if all 7 are and  which tiles are blanks

--- src/pointCalculator.java
+++ src/pointCalculator.java
@@ -1,9 +1,18 @@
 public class pointCalculator {
-    public int pointsForWord(String word, int wordStartX, int wordStartY, int wordEndX, int wordEndY) {
+    public int pointsForWord(String word, char[] blanks, int wordStartX, int wordStartY, int wordEndX, int wordEndY) {
+        int pointsForWord = 0;
 
+        pointsForWord *= wordBoostMultiplicator(wordStartX, wordStartY, wordEndX, wordEndY);
+
+        /*
+        if (usedPlayaLetter.length() > 7)
+            pointsForWord += 50
+         */
+
+        return pointsForWord;
     }
 
-    public int pointsForLetter(char letter, int tripleWordBoosts, int doubleWordBoosts, int letterPositionX, int letterPositionY) {
+    public int pointsForLetter(char letter, int letterX, int letterY) {
 
     }
 
