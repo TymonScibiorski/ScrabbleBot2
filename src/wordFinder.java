588c6f79cb6d6faf82dfa9e8a80ea2c50c0fc053
TymonScibiorski <tym9128@gmail.com>
2025-01-27 22:58:24 +0100

    Rethought the though process powering this code. Current direction isn't persuable, other way of thought has to be created

--- src/wordFinder.java
+++ src/wordFinder.java
@@ -2,4 +2,10 @@ public class wordFinder {
     public String mostProfitableWord(String[][] board) {
 
     }
+
+    /*
+    Provided that  checking if a word is violable and calculating points for has the complexity of n,
+    checking the whole square board with a side length of n will have a complexity of n! ^ 2
+    which for 15 would be 1,7 * 10^24... which is not feasible if the code has 60 seconds for a single board calculation...
+     */
 }
