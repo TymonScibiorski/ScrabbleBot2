361e45eae36bdab871d72f087524b9c5f506012c
TymonScibiorski <tym9128@gmail.com>
2025-01-30 23:55:18 +0100

    Started working on a function that will divide the board into fragments to test (if a given word can be used in that fragment)

--- src/wordFinder.java
+++ src/wordFinder.java
@@ -3,6 +3,11 @@ public class wordFinder {
 
     }
 
+    public int[] fragments(String[][] board) {
+
+        return new int[]{int x, int startY, int endY};
+    }
+
     /*
     Provided that  checking if a word is violable and calculating points for has the complexity of n,
     checking the whole square board with a side length of n will have a complexity of n! ^ 2
